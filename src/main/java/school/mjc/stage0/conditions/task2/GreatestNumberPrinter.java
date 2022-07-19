27b535bfa207d072abf264b4764cf4741f2fcd85
Qodirov Lochinbek <lochinbek433@gmail.com>
2022-07-20 01:13:40 +0500

    Done!

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        //we must use more operator for comparing to numbers
+        if(first > second){
+            System.out.println(first);
+        } else{
+            System.out.println(second);
+        }
     }
 }
